8c1419260b769f90d8d1f8c1fd5c56ad0e81c154
Oskar <kogutoskar123@gmail.com>
2019-12-26 00:34:38 +0100

    Minor changes and test build

---
+++ LevelGenerator/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private int _speed = 100;
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Translate(h * Time.deltaTime * _speed,v * Time.deltaTime * _speed,0);
+    }
+}
