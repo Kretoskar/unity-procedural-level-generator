e0df7a23bf1b312224bb59db96559f684cc248ee
Oskar <kogutoskar123@gmail.com>
2019-12-25 10:53:12 +0100

    Added multiple room spawning

--- LevelGenerator/Assets/Scripts/LevelGenerator.cs
+++ LevelGenerator/Assets/Scripts/LevelGenerator.cs
@@ -23,13 +23,18 @@ namespace game.levelGeneration
         private GameObject _floorPrefab = null;
         private GameObject _wallPrefab = null;
 
+        private int _currentNumberOfRooms = 0;
         private GameObject _levelGO;
         private GameObject _pathGO;
-        private List<Vector2> _takenPositions = new List<Vector2>();
+        private List<Vector2> _emptyPathClosings;
+        private List<Vector2> _takenPositions;
 
         private void Awake()
         {
             InjectDependenciesFromScriptableObject();
+            _emptyPathClosings = new List<Vector2>();
+            _takenPositions = new List<Vector2>();
+            _emptyPathClosings.Add(new Vector2(0, 0));
         }
 
         private void Start()
@@ -43,17 +48,23 @@ namespace game.levelGeneration
             _levelGO.transform.parent = transform;
             _pathGO = new GameObject("Path");
             _pathGO.transform.parent = _levelGO.transform;
-            SpawnRoom();
+            do
+            {
+                for (int j = 0; j < _emptyPathClosings.Count; j++)
+                {
+                    SpawnRoom(_emptyPathClosings[j]);
+                }
+            } while (_currentNumberOfRooms < _numberOfRooms);
         }
 
-        private void SpawnRoom()
+        private void SpawnRoom(Vector2 spawnPosition)
         {
             GameObject roomGO = new GameObject("Room");
             roomGO.transform.parent = _levelGO.transform;
             //Generate random room 
             int roomHeight = UnityEngine.Random.Range(_minRoomHeight, _maxRoomHeight + 1);
             int roomWidth = UnityEngine.Random.Range(_minRoomWidth, _maxRoomWidth + 1);
-            int numberOfDoors = UnityEngine.Random.Range(1, 5);
+            int numberOfDoors = UnityEngine.Random.Range(1, 4);
             List<DoorPosition> doorPositionTypes = new List<DoorPosition>();
             Array values = Enum.GetValues(typeof(DoorPosition)); //Used for getting random value from enum
             for (int i = 0; i < numberOfDoors; i++)
@@ -69,46 +80,59 @@ namespace game.levelGeneration
             {
                 for(int j = 0; j < roomHeight; j++)
                 {
-                    SpawnSingleFloorTile(i, j, roomGO.transform);
+                    SpawnSingleFloorTile(i + (int)spawnPosition.x, j + (int)spawnPosition.y, roomGO.transform);
                 }
             }
+            _currentNumberOfRooms++;
             SpawnPath(room);
         }
 
         private void SpawnPath(Room room)
         {
+            _emptyPathClosings.Clear();
             for(int i = 0; i < room.DoorPositions.Count; i++)
             {
-                for(int j = 0; j < _maxPathLength; j++)
+                if (_currentNumberOfRooms < _numberOfRooms)
                 {
-                    switch(room.DoorPositionTypes[i])
+                    Vector2 pathClosingPosition = new Vector2();
+                    for (int j = 0; j < _maxPathLength; j++)
                     {
-                        case (DoorPosition.Top):
-                            SpawnSingleFloorTile((int)room.DoorPositions[i].x, (int)room.DoorPositions[i].y + j, _pathGO.transform, "PathFloor");
-                            break;
-                        case (DoorPosition.Right):
-                            SpawnSingleFloorTile((int)room.DoorPositions[i].x + j, (int)room.DoorPositions[i].y, _pathGO.transform, "PathFloor");
-                            break;
-                        case (DoorPosition.Bottom):
-                            SpawnSingleFloorTile((int)room.DoorPositions[i].x, (int)room.DoorPositions[i].y - j, _pathGO.transform, "PathFloor");
-                            break;
-                        case (DoorPosition.Left):
-                            SpawnSingleFloorTile((int)room.DoorPositions[i].x - j, (int)room.DoorPositions[i].y, _pathGO.transform, "PathFloor");
-                            break;
+                        Vector2 spawnPosition = new Vector2();
+                        switch (room.DoorPositionTypes[i])
+                        {
+                            case (DoorPosition.Top):
+                                spawnPosition = new Vector2(room.DoorPositions[i].x, room.DoorPositions[i].y + j);                               
+                                break;
+                            case (DoorPosition.Right):
+                                spawnPosition = new Vector2(room.DoorPositions[i].x + j, room.DoorPositions[i].y);
+                                break;
+                            case (DoorPosition.Bottom):
+                                spawnPosition = new Vector2(room.DoorPositions[i].x, room.DoorPositions[i].y - j);
+                                break;
+                            case (DoorPosition.Left):
+                                spawnPosition = new Vector2((int)room.DoorPositions[i].x - j, (int)room.DoorPositions[i].y);
+                                break;
+                        }
+                        pathClosingPosition = spawnPosition;
+                        SpawnSingleFloorTile((int)spawnPosition.x, (int)spawnPosition.y, _pathGO.transform, "PathFloor");
                     }
+                    _emptyPathClosings.Add(pathClosingPosition);
                 }
             }
         }
 
         private void SpawnSingleFloorTile(int xPos, int yPos, Transform parent, string name = "Floor")
         {
-            GameObject floorTile = Instantiate(_floorPrefab, new Vector2(xPos, yPos), Quaternion.identity);
+            Vector2 spawnPosition = new Vector2(xPos, yPos);
+            GameObject floorTile = Instantiate(_floorPrefab, spawnPosition, Quaternion.identity);
+            floorTile.name = name;
             floorTile.transform.parent = parent;
+            _takenPositions.Add(spawnPosition);
         }
 
         private void InjectDependenciesFromScriptableObject()
         {
-            _numberOfRooms = _levelSO.NumberOfRooms;
+            _numberOfRooms = _levelSO.NumberOfRooms + 1;
             _maxRoomHeight = _levelSO.MaxRoomHeight;
             _maxRoomWidth = _levelSO.MaxRoomWidth;
             _minRoomHeight = _levelSO.MinRoomHeight;
